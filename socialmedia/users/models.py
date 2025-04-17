38fff180ec0d5a7b4f752c6ca22c4a6d0f853b7b
Petri Niskanen <AC7887@student.jamk.fi>
2025-04-17 18:01:57 +0300

    SOME-6: Friend request model

--- socialmedia/users/models.py
+++ socialmedia/users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Profile model
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     friends = models.ManyToManyField(User, blank=True, related_name="friend_profiles")
@@ -10,3 +12,39 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+# Friend request model
+class FriendRequest(models.Model):
+
+    # Friend request status
+    STATUS = [
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('declined', 'Declined')
+    ]
+
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_friend_requests")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_friend_requests")
+    status = models.CharField(max_length=10, choices=STATUS, default='pending')
+
+    class Meta:
+        unique_together = ('sender', 'receiver') # Prevent duplicaton
+
+    def accept(self):
+        """Accepts friend request and adds users as friends"""
+        self.status = 'accepted'
+        self.save()
+        self.sender.profile.friends.add(self.receiver)
+        self.receiver.profile.friends.add(self.sender)
+
+    def decline(self):
+        """Declines friend request"""
+        self.status = 'declined'
+        self.save()
+
+    def cancel(self):
+        """Cancels friend request"""
+        self.delete()
+
+    def __str__(self):
+        return f"{self.sender.username} -> {self.receiver.username} ({self.status})"
